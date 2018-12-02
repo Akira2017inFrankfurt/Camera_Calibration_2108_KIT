2a513dea1ee105b8f6300d0ab6ecd6595286112d
jxHHL <44751538+jxHHL@users.noreply.github.com>
2018-12-02 11:52:21 +0100

    delete some folder

--- frame synch/F_dt_8pt.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def F_dt_8pt(xp,x,v)
-    #xp is u1s, x is u2
-    #input xp and x is 8*2 matrix
-    M=[v[]]
--- frame synch/frame2time.py
+++
@@ -1,3 +0,0 @@
-def frame2time(frame,fps,offset)
-    time=offset+frame/fps
-    return time
