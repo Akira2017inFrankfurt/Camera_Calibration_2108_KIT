e82b64810b4d9147c5829a90ad59b7f029e469be
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2018-12-02 16:47:01 +0100

    Update calcuate_epipolar_distance.py

--- Frame synchronization/temp/calcuate_epipolar_distance.py
+++ Frame synchronization/temp/calcuate_epipolar_distance.py
@@ -11,19 +11,31 @@ import math
 
 
 def s_e_d(u1, u2, F):
+    # 3 * 80
     l_2 = np.dot(F, u1)
+    # 3 * 80
     l_1 = np.dot(F.T, u2)
 
-    length_u2 = len(u2)
+    # 3 * 80 = 240 * 1
+    u_2_reshape = np.reshape(u2.T, (len(u2) * len(u2[0], 1)))
+    l_2_reshape = np.reshape(l_2.T, (len(l_2) * len(l_2[0], 1)))
+
+    # 240
+    length_u_2 = len(u_2_reshape)
+
+    # 240 * 1
     ejs = []
-    for i in range(length_u2):
-        ejs.append(u2[i] * l_2[i])
+    for i in range(length_u_2):
+        ejs.append(u_2_reshape[i] * l_2_reshape[i])
 
-    s_l_1_1 = l_1[::3]
-    s_l_1_2 = l_1[1::3]
-    s_l_2_1 = l_2[::3]
-    s_l_2_2 = l_2[1::3]
+    # 1 * 80
+    # s_l_1_1 就是l_1的第一行
+    s_l_1_1 = l_1[0]
+    s_l_1_2 = l_1[1]
+    s_l_2_1 = l_2[0]
+    s_l_2_2 = l_2[1]
 
+    # 1 * 80
     scales = []
     for j in range(len(s_l_1_1)):
         t_1 = pow(s_l_1_1[j], 2) + pow(s_l_1_2[j], 2)
@@ -31,4 +43,11 @@ def s_e_d(u1, u2, F):
         # todo: ensure the value of t_1 is not zero!
         scales.append(1.0 / math.sqrt(t_1) + 1.0 / math.sqrt(t_2))
 
-    e = [sum(ejs) * value for value in scales]
+    temp = np.reshape(ejs, (3, length_u_2 / 3))
+
+    # 取列的和，然后做乘运算，得到结果加入到e中
+    e = []
+    for col_index in range(len(u_2_reshape) / 3):
+        sum = 0
+        sum = temp[0][col_index] + temp[1][col_index] + temp[2][col_index]
+        e.append(sum * scales)
